64b915310b7425fd3a920127b6f2a76bdeb007fa
HafizZada16 <hafizzada16@gmail.com>
2025-10-31 15:03:08 +0700

    Membuat Row Gender

--- app/src/main/java/com/example/questuserinput_016/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_016/ui/theme/TeksEdit.kt
@@ -1,11 +1,16 @@
 package com.example.questuserinput_016.ui.theme
 
 import android.R
+import androidx.compose.animation.core.withInfiniteAnimationFrameMillis
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,7 +40,19 @@ fun FormDataDiri(modifier: Modifier)
             value = textNama,
             singleLine = true,
             shape = MaterialTheme.shapes.large,
-
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                    textNama = it
+            }
         )
+        Row {
+            gender.forEach { item { ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = {}
+                ))
+            }}
+        }
     }
 }
