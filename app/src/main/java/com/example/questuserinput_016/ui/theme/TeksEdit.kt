55f9a9fbaa44ef795ce64373a1b0fa855a59c04a
HafizZada16 <hafizzada16@gmail.com>
2025-10-31 14:53:31 +0700

    Membuat Columns nya

--- app/src/main/java/com/example/questuserinput_016/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_016/ui/theme/TeksEdit.kt
@@ -1,10 +1,19 @@
 package com.example.questuserinput_016.ui.theme
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier)
@@ -16,4 +25,17 @@ fun FormDataDiri(modifier: Modifier)
     var nama by remember {mutableStateOf("")}
     var alamat by remember {mutableStateOf("")}
     var jenis by remember {mutableStateOf("")}
+
+    val gender:List<String> = listOf("Laki-laki","Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+
+        )
+    }
 }
