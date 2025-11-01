3319d974c166de2b2e8495bd69fe1ff00b0b0e5e
HafizZada16 <hafizzada16@gmail.com>
2025-11-01 15:15:17 +0700

    Membuat Scaffold FormInput

--- app/src/main/java/com/example/questuserinput_016/ui/theme/FormTugas.kt
+++ app/src/main/java/com/example/questuserinput_016/ui/theme/FormTugas.kt
@@ -1,9 +1,17 @@
 package com.example.questuserinput_016.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -11,4 +19,17 @@ fun Forminput() {
     val gradientBrush = Brush.verticalGradient(
         colors = listOf(Color(0xFFE0F7FA), Color(0xFFA5D6A7), Color(0xFFC8E6C9))
     )
+
+    Scaffold { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(gradientBrush)
+                .padding(padding)
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            RegistrationForm()
+        }
+    }
 }
